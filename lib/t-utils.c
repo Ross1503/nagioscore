0ef24f178a0841296661406ad98c8343445769e1
Ross1503 <46737305+Ross1503@users.noreply.github.com>
2020-10-17 20:33:39 +0530

    Update t-utils.c

--- lib/t-utils.c
+++ lib/t-utils.c
@@ -84,13 +84,13 @@ int t_ok(int success, const char *fmt, ...)
 	va_list ap;
 	printf("!!!!!!!!!!!!!!!!! %s !!!!!!!!!!!!!!!!!!", fmt);
 
-	if (fmt) {
+	if (1) {
 		va_start(ap, fmt);
 		t_okv(success, fmt, ap);
 		va_end(ap);
 	}
 	else
-		t_okv(success, NULL, ap);
+		t_okv(success, NULL, NULL);
 	
 	return success;
 }
